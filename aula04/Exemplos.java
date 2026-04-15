54608c497fc8ee6e106f6cff6d8b7485b66eb9f4
Maria Carolina Sayo Ogassawara Freitas <ren.setsunaaa@gmail.com>
2026-04-15 18:21:14 -0300

    estudos realizados, conclusões feitas
    Added examples of long, string, and float variable usage.

--- aula04/Exemplos.java
+++ aula04/Exemplos.java
@@ -2,7 +2,15 @@ package aula04;
 
 public class Exemplos {
     public static void main(String[] args) {
-      long myNum = 15000000000L;
-      System.out.println(myNum);  
+        long numero_teste = 38181237817491;
+        System.out.println("um número aleatório: " + numero_teste);
+//string
+    string oque_a_carol_quer = "certezas";
+    system.out.println("o que a carol quer mesmo?: " + oque_a_carol_quer);
+
+//float 
+        float testeFloat = 10f;
+        system.out.println(testeFloat);
+        
     }
   }
