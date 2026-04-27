f5f6f71f82ca838c111b5ff97458f776180ae101
M Carolina Sayo O Freitas <astersmistake@gmail.com>
2026-04-27 09:25:25 -0300

    aula bem sucedida

---
+++ aula05/Aula5.java
@@ -0,0 +1,9 @@
+public class Aula5 {
+  public static void main(String[] args) {
+    int myfirstInt = 9;
+    double myfirstDouble = myfirstInt; // faz com que o sistema leia o número com , !!!
+
+    System.out.println(myfirstInt);
+    System.out.println(myfirstDouble);
+  }
+}
---
+++ aula05/operadores.java
@@ -0,0 +1,28 @@
+public class operadores {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+
+    System.out.println(x + y); // resulta em 13
+    System.out.println(x - y); // resulta em 7
+    System.out.println(x * y); // resulta em 30
+    System.out.println(x / y); // resulta em 3
+    System.out.println(x % y); // resulta em 1
+
+    int z = 5;
+    ++z;
+    System.out.println(z); // = 6
+    --z;
+    System.out.println(z); // = 4
+    
+    int a = 10;
+    int b = 7;
+    
+    System.out.println(a / b);   // Integer division, result is 3
+
+    double c = 10.0d;
+    double d = 3.0d;
+    System.out.println(c / d);
+
+    }
+}
