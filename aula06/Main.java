fdce5e732fde5c37696d7cbe61c82c80adf329d3
M Carolina Sayo O Freitas <astersmistake@gmail.com>
2026-05-04 09:19:57 -0300

    v1 da aula06

---
+++ aula06/Main.java
@@ -0,0 +1,9 @@
+package aula06;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x > 1 && x < 6); // retorna como verdadeiro por simplesmente ser verdadeiro!
+        
+    }
+}
